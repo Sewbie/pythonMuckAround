c475bc002e6ae82622364a37589a369a70f9bcea
Sewbie <subaru_muroi@hotmail.com>
2021-08-19 13:10:20 +1000

    Added for loops examples

--- day2 git and python.py
+++ day2 git and python.py
@@ -19,6 +19,56 @@ pressure.append([0.43, 2.7, 0.665])
 
 print(pressure)
 
+
+#this merges list into one list
 pressure1 = [0.27, 0.456, 0.33333, 0.7, 1.4]
 
 pressure1.extend([0.43, 2.7, 0.665])
+
+
+
+
+#delete
+del pressure1[0]
+
+empty=[1,"rrrrrr", 1.2345]
+
+del empty[1]
+
+
+#for loops
+subjects = ['cats', 'dogs', 'goldfish', 'Jacob']
+
+for i in subjects:  
+    print('I really like', i)
+    
+
+for number in [2,3,4]:
+    print('the value of number x=', number*2)
+
+#all indented lines are included in loop
+print('Subaru')
+
+
+
+
+# primes = [2,3,5]
+# for p in primes:
+#         squared = p **2
+#         cubed = p **3
+#         print('original:',p,'squared:',squared,'cubed:', cubed)
+        
+        
+primes = [2,3,5]
+squared=[]
+cubed=[]
+
+for p in primes:
+    sqr=p**2
+    squared.append(sqr)
+    cbd=p**3
+    cubed.append(cbd)
+
+
+for i in range(0,len(primes),1):
+    print('the value of i in this loop is:', primes[i])
